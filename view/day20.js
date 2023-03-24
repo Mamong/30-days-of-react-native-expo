f261aa2ed8bf7cd7e946dbe3e78469527d61781c
mamong <1054209736@qq.com>
2023-03-24 18:46:33 +0800

    add day20 and adjust day21

--- view/day20.js
+++ view/day20.js
@@ -1,2 +1,222 @@
-export default function Day20() {
+/**
+ * Day 20
+ * Reminder
+ */
+'use strict';
+
+import React, { useState, useRef, useEffect } from 'react';
+import { Image, StyleSheet, StatusBar, Text, TextInput, LayoutAnimation, TouchableHighlight, View } from 'react-native';
+import Util from './utils';
+// import {BlurView,VibrancyView} from 'react-native-blur';
+import { Ionicons } from '@expo/vector-icons';
+
+export const ReminderContainer = ({ listData, listStyle, onSwitch }) => {
+    const [list, setList] = useState(listData.list ?? [])
+    const addList = useRef(null)
+    const { title = "提醒事项", theme = "#fe952b" } = listData
+
+    const animations = {
+        duration: 200,
+        create: {
+            type: LayoutAnimation.Types.linear,
+        },
+        update: {
+            type: LayoutAnimation.Types.linear,
+            springDamping: 0.7,
+        },
+    };
+
+    const _done = (index) => {
+        const newList = list.slice();
+        newList[index].selected = !newList[index].selected;
+        setList(newList)
+        LayoutAnimation.configureNext(animations);
+    }
+
+    const _addList = (text) => {
+        const newList = list.slice();
+        newList.push({
+            selected: false,
+            text
+        })
+        addList.current.setNativeProps({ text: '' });
+        setList(newList)
+    }
+
+    const items = list.map((elem, index) => {
+        return (
+            <View key={index} style={[styles.reminderList, { opacity: elem.selected ? 0.5 : 1 }]}>
+                <TouchableHighlight underlayColor="transparent" style={[styles.check, { borderColor: elem.selected ? theme : "#c6c6c6" }]} 
+                onPress={() => _done(index)}>
+                    <View style={elem.selected ? [styles.fill, { backgroundColor: theme }] : null}></View>
+                </TouchableHighlight>
+                <View style={styles.input}>
+                    <TextInput defaultValue={elem.text} style={styles.inputText} />
+                </View>
+            </View>
+        );
+    })
+    items.push(
+        <View key="add" style={styles.reminderList}>
+            <View style={styles.add}>
+                <Ionicons name="md-add" style={styles.addIcon} color="#c6c6c6" size={22} />
+            </View>
+            <View style={styles.input}>
+                <TextInput autoCapitalize="none" ref={addList} onBlur={(event) => _addList(event.nativeEvent.text)} style={styles.inputText} />
+            </View>
+        </View>
+    );
+    return (
+        <View style={[styles.reminderContainer, listStyle]}>
+            <Image style={styles.reminderBg} source={require('./img/packed.png')} />
+            <View style={styles.reminderContent}>
+                <TouchableHighlight underlayColor="transparent" onPress={onSwitch}>
+                    <View style={styles.reminderTitleContainer}>
+                        <Text style={[styles.reminderTitle, { color: theme }]}>{title}</Text>
+                        <Text style={[styles.reminderTitle, { color: theme }]}>{list.length}</Text>
+                    </View>
+                </TouchableHighlight>
+                <View style={styles.reminderListContainer}>
+                    {items}
+                </View>
+            </View>
+        </View>
+    );
 }
+
+export default function Day20() {
+
+    const listData = {
+        title: "Development",
+        theme: "#fe952b",
+        list: [{
+            selected: false,
+            text: "day20",
+        }, {
+            selected: false,
+            text: "day21",
+        }, {
+            selected: false,
+            text: "day22",
+        }, {
+            selected: false,
+            text: "day23",
+        }, {
+            selected: false,
+            text: "day24",
+        }, {
+            selected: false,
+            text: "day25",
+        }],
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image source={require('./img/desktop.png')} style={styles.container}>
+            </Image>
+            <ReminderContainer listData={listData} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: Util.size.height,
+        width: Util.size.width,
+    },
+    reminderContainer: {
+        height: Util.size.height - 65,
+        width: Util.size.width,
+        borderRadius: 10,
+        position: "absolute",
+        top: 20,
+        left: 0,
+        backgroundColor: "#fff",
+        shadowColor: "#000",
+        shadowOpacity: 0.3,
+        shadowRadius: 2,
+        shadowOffset: {
+            height: -1,
+            width: 0,
+        }
+    },
+    reminderBg: {
+        height: Util.size.height - 65,
+        width: Util.size.width,
+        borderRadius: 10,
+        resizeMode: "cover",
+        opacity: 0.5,
+    },
+    reminderContent: {
+        height: Util.size.height - 65,
+        width: Util.size.width,
+        backgroundColor: "transparent",
+        position: "absolute",
+        top: 0,
+        left: 0,
+    },
+    reminderTitleContainer: {
+        height: 65,
+        width: Util.size.width,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingLeft: 15,
+        paddingRight: 15,
+        alignItems: "center"
+    },
+    reminderTitle: {
+        fontSize: 28,
+        fontWeight: "300",
+        textShadowColor: "#ccc",
+        textShadowOffset: { width: 0, height: 1, },
+        textShadowRadius: 1,
+    },
+    reminderListContainer: {
+        flex: 1,
+        borderTopColor: "#ccc",
+        borderTopWidth: 1,
+    },
+    reminderList: {
+        flexDirection: "row",
+        paddingLeft: 15,
+        height: 45,
+        width: Util.size.width,
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    check: {
+        backgroundColor: "transparent",
+        borderWidth: 1,
+        borderColor: "#c6c6c6",
+        width: 22,
+        height: 22,
+        borderRadius: 11,
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowRadius: 1,
+        shadowColor: '#aaa',
+        shadowOpacity: 0.3,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    fill: {
+        width: 16,
+        height: 16,
+        borderRadius: 8,
+    },
+    input: {
+        width: Util.size.width - 50,
+        height: 45,
+        borderBottomWidth: 1,
+        borderBottomColor: "#ccc",
+    },
+    inputText: {
+        height: 43,
+        color: "#363636",
+    },
+    addIcon: {
+        paddingLeft: 5
+    }
+});
--- view/day21.js
+++ view/day21.js
@@ -1,2 +1,129 @@
+/**
+ * Day 21
+ * 
+ */
+'use strict';
+
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, StatusBar, TouchableHighlight, LayoutAnimation, View } from 'react-native';
+import Util from './utils';
+import { ReminderContainer } from './day20';
+import {
+    useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
 export default function Day21() {
+
+    const listData = [{
+        title: "Scheduled",
+        theme: "#979797",
+        list: [],
+    }, {
+        title: "Movie",
+        theme: "#cb7adf",
+        list: [],
+    }, {
+        title: "Work",
+        theme: "#f9005f",
+        list: [],
+    }, {
+        title: "Home",
+        theme: "#00a8f4",
+        list: [],
+    }, {
+        title: "Reminder",
+        theme: "#68d746",
+        list: [],
+    }, {
+        title: "Development",
+        theme: "#fe952b",
+        list: [{
+            selected: false,
+            text: "day20",
+        }, {
+            selected: false,
+            text: "day21",
+        }, {
+            selected: false,
+            text: "day22",
+        }, {
+            selected: false,
+            text: "day23",
+        }, {
+            selected: false,
+            text: "day24",
+        }, {
+            selected: false,
+            text: "day25",
+        }],
+    }];
+
+    const animations = {
+        duration: 200,
+        create: {
+            type: LayoutAnimation.Types.linear,
+        },
+        update: {
+            type: LayoutAnimation.Types.linear,
+            springDamping: 0.5,
+        },
+    };
+
+    const [isOn, setIsOn] = useState(Array(listData.length).fill(false))
+    const [init, setInit] = useState(true)
+
+    const _switch = (index) => {
+        const isOn = listData.map(() => {
+            return false;
+        });
+        isOn[index] = true;
+        setIsOn(isOn)
+        setInit(false)
+        LayoutAnimation.configureNext(animations);
+    }
+
+    const _reset = () => {
+        const isOn = listData.map(() => {
+            return false;
+        });
+        setIsOn(isOn)
+        setInit(true)
+        LayoutAnimation.configureNext(animations);
+    }
+
+    const insets = useSafeAreaInsets();
+    const top = 20 + insets.top*0
+    const len = listData.length;
+    const lists = listData.map((elem, index) => {
+        return <ReminderContainer
+            onSwitch={() => _switch(index)}
+            key={"list" + index}
+            listData={elem}
+            listStyle={init ? { top: top + index * 65 } : { top: isOn[index] ? top : Util.size.height + 5 * index - 5 * len }} />;
+    })
+
+    return (
+        <View style={styles.container}>
+            <Image source={require('./img/desktop.png')} style={styles.container}>
+            </Image>
+            {lists}
+            <TouchableHighlight underlayColor="transparent" style={styles.reset} onPress={_reset}>
+                <View></View>
+            </TouchableHighlight>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        height: Util.size.height,
+        width: Util.size.width,
+    },
+    reset: {
+        height: 30,
+        width: Util.size.width,
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+    }
+});
